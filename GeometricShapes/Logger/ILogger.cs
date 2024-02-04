c1aad94314ad070f3d913b98ba03ce4f2c755365
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-02-04 11:47:41 +0100

    added: implemented SRP

--- GeometricShapes/Logger/ILogger.cs
+++ GeometricShapes/Logger/ILogger.cs
@@ -1,6 +1,8 @@
-﻿namespace GeometricShapes;
-
-public interface ILogger
+﻿namespace GeometricShapes.Logger
 {
-
+    // Single Responsibility Principle (SRP): Interface for logging mechanism
+    public interface ILogger
+    {
+        void Log(string message);
+    }
 }
