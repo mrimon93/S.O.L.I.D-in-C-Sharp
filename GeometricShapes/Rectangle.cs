3ce6776340199f4b16e4cf1a4202cc1c9a63168e
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-02-04 11:40:42 +0100

    added:  |SRP & LSP & OCP|

--- GeometricShapes/Rectangle.cs
+++ GeometricShapes/Rectangle.cs
@@ -1,6 +1,22 @@
-﻿namespace GeometricShapes;
-
-public class Rectangle
+﻿namespace GeometricShapes
 {
+    // Single Responsibility Principle (SRP): Represents a rectangle
+    public class Rectangle : Shape
+    {
+        // Liskov Substitution Principle (LSP): Specialized version of Shape
+        public double Width { get; set; }
+        public double Height { get; set; }
 
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        // Open/Closed Principle (OCP): Overrides the Area method for rectangle-specific calculation
+        public override double Area()
+        {
+            return Width * Height;
+        }
+    }
 }
