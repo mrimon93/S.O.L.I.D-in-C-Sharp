097ebce0e882c5a396c80397016a8b3f31dd9848
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-02-05 19:15:28 +0100

    Adding: the comment

--- GeometricShapes/Logger/ConsoleLogger.cs
+++ GeometricShapes/Logger/ConsoleLogger.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 
 namespace GeometricShapes.Logger
-{ 
+{
+    // Single Responsibility Principle (SRP): Console logging implementation
     public class ConsoleLogger : ILogger
     {
         public void Log(string message)
@@ -10,5 +11,3 @@ namespace GeometricShapes.Logger
         }
     }
 }
-
-
