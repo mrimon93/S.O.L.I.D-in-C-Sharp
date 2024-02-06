3d4c2de9b1c68bc240c7d805540c47f207f2df88
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-02-06 18:44:47 +0100

    Setup: For Test

--- GeometricShapesTests/ShapeTests/ShapeTests.cs
+++ GeometricShapesTests/ShapeTests/ShapeTests.cs
@@ -6,8 +6,13 @@ namespace GeometricShapesTests;
 public class ShapeTests
 {
     [Fact]
-    public void Test1()
+    public void Shape_Area_Default_ReturnsZero()
     {
+        //Arrange
+
+        //Act
+
+        // Assert
 
     }
 }
